b9c401547d4346487188b5365f1481a27d7a36f1
Joel Messerli <hi.github@peg.nu>
2018-02-24 01:06:45 +0100

    Fix Kotlin 1.2 / Jackson-Kotlin incompatibility issue

--- src/test/kotlin/nu/peg/web/files/PegnuFilesApplicationTests.kt
+++ src/test/kotlin/nu/peg/web/files/PegnuFilesApplicationTests.kt
@@ -1,6 +1,5 @@
 package nu.peg.web.files
 
-import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
@@ -11,7 +10,6 @@ import org.springframework.test.context.junit4.SpringRunner
 class PegnuFilesApplicationTests {
 
     @Test
-    @Ignore
     fun contextLoads() {
     }
 
