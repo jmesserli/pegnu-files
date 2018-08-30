8f4ad140cfbb442ca850e17f759163fddcf3b42c
Joel Messerli <hi.github@peg.nu>
2018-08-30 16:55:46 +0200

    Start integrating keycloak

--- src/main/kotlin/nu/peg/web/files/security/SecurityConfig.kt
+++ src/main/kotlin/nu/peg/web/files/security/SecurityConfig.kt
@@ -1,26 +1,71 @@
 package nu.peg.web.files.security
 
+import org.keycloak.adapters.KeycloakConfigResolver
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter
+import org.keycloak.adapters.springsecurity.filter.KeycloakAuthenticatedActionsFilter
+import org.keycloak.adapters.springsecurity.filter.KeycloakAuthenticationProcessingFilter
+import org.keycloak.adapters.springsecurity.filter.KeycloakPreAuthActionsFilter
+import org.keycloak.adapters.springsecurity.filter.KeycloakSecurityContextRequestFilter
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.core.session.SessionRegistryImpl
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy
+
 
 @Configuration
-class WebSecurityConfig : WebSecurityConfigurerAdapter() {
+class WebSecurityConfig : KeycloakWebSecurityConfigurerAdapter() {
+    @Bean
+    fun keycloakConfigResolver(): KeycloakConfigResolver {
+        return KeycloakSpringBootConfigResolver()
+    }
+
+    @Bean
+    fun keycloakAuthenticationProcessingFilterRegistrationBean(
+            filter: KeycloakAuthenticationProcessingFilter): FilterRegistrationBean<*> {
+        val registrationBean = FilterRegistrationBean(filter)
+        registrationBean.isEnabled = false
+        return registrationBean
+    }
+
+    @Bean
+    fun keycloakPreAuthActionsFilterRegistrationBean(
+            filter: KeycloakPreAuthActionsFilter): FilterRegistrationBean<*> {
+        val registrationBean = FilterRegistrationBean(filter)
+        registrationBean.isEnabled = false
+        return registrationBean
+    }
+
+    @Bean
+    fun keycloakAuthenticatedActionsFilterBean(
+            filter: KeycloakAuthenticatedActionsFilter): FilterRegistrationBean<*> {
+        val registrationBean = FilterRegistrationBean(filter)
+        registrationBean.isEnabled = false
+        return registrationBean
+    }
+
+    @Bean
+    fun keycloakSecurityContextRequestFilterBean(
+            filter: KeycloakSecurityContextRequestFilter): FilterRegistrationBean<*> {
+        val registrationBean = FilterRegistrationBean(filter)
+        registrationBean.isEnabled = false
+        return registrationBean
+    }
+
+    override fun configure(auth: AuthenticationManagerBuilder?) {
+        auth?.authenticationProvider(keycloakAuthenticationProvider())
+    }
+
+    override fun sessionAuthenticationStrategy(): SessionAuthenticationStrategy {
+        return RegisterSessionAuthenticationStrategy(SessionRegistryImpl())
+    }
+
     override fun configure(http: HttpSecurity) {
-        http.formLogin()
-                .loginProcessingUrl("/do-login")
-                .defaultSuccessUrl("/", true)
-                .successHandler({ request, response, _ ->
-                    val path = request.getParameter("path")
-                    response.sendRedirect("/files?path=$path")
-                })
-                .failureUrl("/auth_error")
-                .and()
-                .logout()
-                .logoutUrl("/do-logout")
-                .logoutSuccessUrl("/")
-                .and()
-                .authorizeRequests()
+        http.authorizeRequests()
                 .antMatchers("/", "/files", "/download").permitAll()
                 .anyRequest().hasRole("USER")
     }
