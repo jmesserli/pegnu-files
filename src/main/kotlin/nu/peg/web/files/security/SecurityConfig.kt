966c39e84fd0c581173eac18f7d209d5a0673031
Joel Messerli <hi.github@peg.nu>
2021-02-01 21:03:21 +0100

    Revert to keycloak adapter

--- src/main/kotlin/nu/peg/web/files/security/SecurityConfig.kt
+++ src/main/kotlin/nu/peg/web/files/security/SecurityConfig.kt
@@ -1,18 +1,38 @@
 package nu.peg.web.files.security
 
-import org.springframework.context.annotation.Configuration
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter
+import org.springframework.context.annotation.Bean
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper
+import org.springframework.security.core.session.SessionRegistryImpl
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy
 
-@Configuration
-class SecurityConfig : WebSecurityConfigurerAdapter() {
-    override fun configure(http: HttpSecurity?) {
-        http!!.authorizeRequests()
-            .antMatchers("/createFolder").hasAuthority(SecurityUtil.Role.MAKE_DIRECTORY)
-            .antMatchers("/uploadFile").hasAuthority(SecurityUtil.Role.UPLOAD)
-            .antMatchers("/deleteFile").hasAuthority(SecurityUtil.Role.DELETE)
+@KeycloakConfiguration
+class SecurityConfig : KeycloakWebSecurityConfigurerAdapter() {
+    override fun configure(http: HttpSecurity) {
+        super.configure(http)
+
+        http.authorizeRequests()
+            .antMatchers("/createFolder").hasRole(SecurityUtil.Role.MAKE_DIRECTORY)
+            .antMatchers("/uploadFile").hasRole(SecurityUtil.Role.UPLOAD)
+            .antMatchers("/deleteFile").hasRole(SecurityUtil.Role.DELETE)
             .anyRequest().permitAll()
-            .and()
-            .oauth2Login()
     }
+
+    override fun configure(auth: AuthenticationManagerBuilder) {
+        val provider = keycloakAuthenticationProvider()
+        provider.setGrantedAuthoritiesMapper(SimpleAuthorityMapper())
+        auth.authenticationProvider(provider)
+    }
+
+    override fun sessionAuthenticationStrategy(): SessionAuthenticationStrategy {
+        return RegisterSessionAuthenticationStrategy(SessionRegistryImpl())
+    }
+
+    @Bean
+    fun keycloakConfigResolver() = KeycloakSpringBootConfigResolver()
 }
