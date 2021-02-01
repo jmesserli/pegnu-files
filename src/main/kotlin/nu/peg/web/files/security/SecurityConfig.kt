49092681a4b60d08eca08f0f60e307c2d4b75fd2
Joel Messerli <hi.github@peg.nu>
2021-02-01 16:19:57 +0100

    Begin updating dependencies & gradle script

--- src/main/kotlin/nu/peg/web/files/security/SecurityConfig.kt
+++ src/main/kotlin/nu/peg/web/files/security/SecurityConfig.kt
@@ -1,79 +1,79 @@
 package nu.peg.web.files.security
 
-import org.keycloak.adapters.KeycloakConfigResolver
-import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver
-import org.keycloak.adapters.springsecurity.KeycloakConfiguration
-import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter
-import org.keycloak.adapters.springsecurity.filter.KeycloakAuthenticatedActionsFilter
-import org.keycloak.adapters.springsecurity.filter.KeycloakAuthenticationProcessingFilter
-import org.keycloak.adapters.springsecurity.filter.KeycloakPreAuthActionsFilter
-import org.keycloak.adapters.springsecurity.filter.KeycloakSecurityContextRequestFilter
-import org.springframework.boot.web.servlet.FilterRegistrationBean
-import org.springframework.context.annotation.Bean
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
-import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.core.session.SessionRegistryImpl
-import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy
-import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy
-
-
-@KeycloakConfiguration
-class WebSecurityConfig : KeycloakWebSecurityConfigurerAdapter() {
-    @Bean
-    fun keycloakConfigResolver(): KeycloakConfigResolver {
-        return KeycloakSpringBootConfigResolver()
-    }
-
-    @Bean
-    fun keycloakAuthenticationProcessingFilterRegistrationBean(
-            filter: KeycloakAuthenticationProcessingFilter): FilterRegistrationBean<*> {
-        val registrationBean = FilterRegistrationBean(filter)
-        registrationBean.isEnabled = false
-        return registrationBean
-    }
-
-    @Bean
-    fun keycloakPreAuthActionsFilterRegistrationBean(
-            filter: KeycloakPreAuthActionsFilter): FilterRegistrationBean<*> {
-        val registrationBean = FilterRegistrationBean(filter)
-        registrationBean.isEnabled = false
-        return registrationBean
-    }
-
-    @Bean
-    fun keycloakAuthenticatedActionsFilterBean(
-            filter: KeycloakAuthenticatedActionsFilter): FilterRegistrationBean<*> {
-        val registrationBean = FilterRegistrationBean(filter)
-        registrationBean.isEnabled = false
-        return registrationBean
-    }
-
-    @Bean
-    fun keycloakSecurityContextRequestFilterBean(
-            filter: KeycloakSecurityContextRequestFilter): FilterRegistrationBean<*> {
-        val registrationBean = FilterRegistrationBean(filter)
-        registrationBean.isEnabled = false
-        return registrationBean
-    }
-
-    override fun configure(auth: AuthenticationManagerBuilder?) {
-        auth?.authenticationProvider(keycloakAuthenticationProvider())
-    }
-
-    override fun sessionAuthenticationStrategy(): SessionAuthenticationStrategy {
-        return RegisterSessionAuthenticationStrategy(SessionRegistryImpl())
-    }
-
-    override fun configure(http: HttpSecurity) {
-        super.configure(http)
-
-        http.authorizeRequests()
-                .antMatchers("/createFolder")
-                .hasAuthority(SecurityUtil.Role.MAKE_DIRECTORY)
-                .antMatchers("/uploadFile")
-                .hasAuthority(SecurityUtil.Role.UPLOAD)
-                .antMatchers("/deleteFile")
-                .hasAuthority(SecurityUtil.Role.DELETE)
-                .anyRequest().permitAll()
-    }
-}
+//import org.keycloak.adapters.KeycloakConfigResolver
+//import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver
+//import org.keycloak.adapters.springsecurity.KeycloakConfiguration
+//import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter
+//import org.keycloak.adapters.springsecurity.filter.KeycloakAuthenticatedActionsFilter
+//import org.keycloak.adapters.springsecurity.filter.KeycloakAuthenticationProcessingFilter
+//import org.keycloak.adapters.springsecurity.filter.KeycloakPreAuthActionsFilter
+//import org.keycloak.adapters.springsecurity.filter.KeycloakSecurityContextRequestFilter
+//import org.springframework.boot.web.servlet.FilterRegistrationBean
+//import org.springframework.context.annotation.Bean
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity
+//import org.springframework.security.core.session.SessionRegistryImpl
+//import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy
+//import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy
+//
+//
+//@KeycloakConfiguration
+//class WebSecurityConfig : KeycloakWebSecurityConfigurerAdapter() {
+//    @Bean
+//    fun keycloakConfigResolver(): KeycloakConfigResolver {
+//        return KeycloakSpringBootConfigResolver()
+//    }
+//
+//    @Bean
+//    fun keycloakAuthenticationProcessingFilterRegistrationBean(
+//            filter: KeycloakAuthenticationProcessingFilter): FilterRegistrationBean<*> {
+//        val registrationBean = FilterRegistrationBean(filter)
+//        registrationBean.isEnabled = false
+//        return registrationBean
+//    }
+//
+//    @Bean
+//    fun keycloakPreAuthActionsFilterRegistrationBean(
+//            filter: KeycloakPreAuthActionsFilter): FilterRegistrationBean<*> {
+//        val registrationBean = FilterRegistrationBean(filter)
+//        registrationBean.isEnabled = false
+//        return registrationBean
+//    }
+//
+//    @Bean
+//    fun keycloakAuthenticatedActionsFilterBean(
+//            filter: KeycloakAuthenticatedActionsFilter): FilterRegistrationBean<*> {
+//        val registrationBean = FilterRegistrationBean(filter)
+//        registrationBean.isEnabled = false
+//        return registrationBean
+//    }
+//
+//    @Bean
+//    fun keycloakSecurityContextRequestFilterBean(
+//            filter: KeycloakSecurityContextRequestFilter): FilterRegistrationBean<*> {
+//        val registrationBean = FilterRegistrationBean(filter)
+//        registrationBean.isEnabled = false
+//        return registrationBean
+//    }
+//
+//    override fun configure(auth: AuthenticationManagerBuilder?) {
+//        auth?.authenticationProvider(keycloakAuthenticationProvider())
+//    }
+//
+//    override fun sessionAuthenticationStrategy(): SessionAuthenticationStrategy {
+//        return RegisterSessionAuthenticationStrategy(SessionRegistryImpl())
+//    }
+//
+//    override fun configure(http: HttpSecurity) {
+//        super.configure(http)
+//
+//        http.authorizeRequests()
+//                .antMatchers("/createFolder")
+//                .hasAuthority(SecurityUtil.Role.MAKE_DIRECTORY)
+//                .antMatchers("/uploadFile")
+//                .hasAuthority(SecurityUtil.Role.UPLOAD)
+//                .antMatchers("/deleteFile")
+//                .hasAuthority(SecurityUtil.Role.DELETE)
+//                .anyRequest().permitAll()
+//    }
+//}
